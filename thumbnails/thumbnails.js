2a5547eb8d4efa7a4d49f14204ff68b93e91b8ec
Aaron Cordovez <aaron.cordovez.m@gmail.com>
2014-11-13 17:18:12 -0800

    Finish Thumbnails

--- thumbnails/thumbnails.js
+++ thumbnails/thumbnails.js
@@ -7,10 +7,9 @@ $.Thumbnails = function(el){
   this.gutterIdx = 0;
   this.$gutter = this.$el.find(".gutter-images")
   this.fillGutterImages();
-  this.$images.on('mouseenter', this.mouseEnter.bind(this));
-  this.$images.on('mouseleave', this.mouseLeave.bind(this));
-  this.$el.on('click', 'img', this.clickCallback.bind(this));
 
+  this.$el.on('click', 'img', this.clickCallback.bind(this));
+  this.$el.on('click', 'a', this.clickNav.bind(this));
 
 };
 
@@ -19,8 +18,8 @@ $.Thumbnails.prototype.fillGutterImages = function () {
   for (var i = this.gutterIdx; i < (this.gutterIdx + 5); i++) {
     this.$gutter.append(this.$images.eq(i));
   }
-
-  
+  this.$gutter.find('img').on('mouseenter', this.mouseEnter.bind(this));
+  this.$gutter.find('img').on('mouseleave', this.mouseLeave.bind(this));
 }
 
 $.Thumbnails.prototype.activate = function($img){
@@ -30,6 +29,15 @@ $.Thumbnails.prototype.activate = function($img){
 $.Thumbnails.prototype.clickCallback = function (event) {
   this.$activeImage = $(event.currentTarget);
   this.activate(this.$activeImage);
+}
+
+$.Thumbnails.prototype.clickNav = function (event) {
+  if ($(event.currentTarget).data("dir") === "right") {
+      this.gutterIdx += 1;
+  } else if ($(event.currentTarget).data("dir") === "left") {
+      this.gutterIdx -= 1;    
+  }
+  this.fillGutterImages();
 }
 
 $.Thumbnails.prototype.mouseEnter = function (event) {
